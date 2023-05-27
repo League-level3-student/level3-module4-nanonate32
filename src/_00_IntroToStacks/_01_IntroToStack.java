6779ead521ac85676570a065e11ec71c35b954bb
natha <natha@RazorSharp.lan>
2023-05-27 16:18:48 -0700

    IntroToStack

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -4,16 +4,19 @@ import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JOptionPane;
-
+import java.util.Stack;
 public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+               Stack<Double> stack = new Stack<Double>();
+               Random random = new Random();
         // 2. Use a loop to push 100 (5x6) random doubles between 00 and 100.0 to the Stack.
-
+                 for (int i = 0; i < 100; i++) {
+					stack.push(random.nextDouble(100));
+				}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusiveity.
-
+                 JOptionPane.showInputDialog("Enter in two numbers between 0 and 100");
         // 4. Pop most of the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, maybe print it to the screen.
 
