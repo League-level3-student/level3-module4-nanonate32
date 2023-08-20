fd3bc3aae91f1d4dc9b695bb9132530970cde97b
jereycui2008 <jeremycuid@gmail.com>
2023-08-19 17:55:04 -0700

    HangMan

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,5 +1,18 @@
 package _03_Hangman;
 
-public class Hangman {
+import java.util.Stack;
 
+import javax.swing.JOptionPane;
+
+public class Hangman {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		String numbers = JOptionPane.showInputDialog("How many numbers?");
+		int num = Integer.parseInt(numbers);
+		int words = Utilities.getTotalWordsInFile("dictionary.txt");
+		stack.push(words);
+		
+		
+	}
+         
 }
