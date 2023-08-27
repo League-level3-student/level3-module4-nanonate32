fa574173990eef3964c68b00e9e1ee8d8458a9e2
jereycui2008 <jeremycuid@gmail.com>
2023-08-26 17:57:57 -0700

    Hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -2,17 +2,28 @@ package _03_Hangman;
 
 import java.util.Stack;
 
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class Hangman {
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<>();
+		Stack<String> stack = new Stack<>();
 		String numbers = JOptionPane.showInputDialog("How many numbers?");
 		int num = Integer.parseInt(numbers);
 		int words = Utilities.getTotalWordsInFile("dictionary.txt");
-		stack.push(words);
+		for (int i = 0; i < num; i++) {
+			String word = Utilities.readRandomLineFromFile("dictionary.txt");
 		
+		if(stack.contains(word)) {
+			i--;
+			continue;
+		}
+		stack.push(word);
 		
+		}
+		stack.pop();
+		JLabel label = new JLabel();
+		label.setText(_ _ _ _ _);
 	}
          
 }
