08c58107b5f6d6c84794803913a334237698d393
league <league@imac-38.attlocal.net>
2023-09-02 17:55:36 -0700

    Hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -2,8 +2,7 @@ package _03_Hangman;
 
 import java.util.Stack;
 
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class Hangman {
 	public static void main(String[] args) {
@@ -11,19 +10,34 @@ public class Hangman {
 		String numbers = JOptionPane.showInputDialog("How many numbers?");
 		int num = Integer.parseInt(numbers);
 		int words = Utilities.getTotalWordsInFile("dictionary.txt");
+		String word = Utilities.readRandomLineFromFile("dictionary.txt");
 		for (int i = 0; i < num; i++) {
-			String word = Utilities.readRandomLineFromFile("dictionary.txt");
+			
 		
 		if(stack.contains(word)) {
-			i--;
+			
 			continue;
+			
 		}
 		stack.push(word);
 		
 		}
 		stack.pop();
-		JLabel label = new JLabel();
-		label.setText(_ _ _ _ _);
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(); 
+		String text = "";
+		for (int i = 0; i < word.length(); i++) {
+			
+			text = text + "_ ";
+		}
+		label.setText(text);
+		frame.add(panel);
+		panel.add(label);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
 	}
-         
+	
+        
 }
